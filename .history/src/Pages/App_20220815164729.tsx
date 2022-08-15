e647738bc90499c5149672ece0e547bdf02f9bf7
Mariana Luiza Vitor Alves Silva <mariana.luiza@spcbrasil.org.br>
2022-08-15 16:49:18 -0300

    add react-router-dom

---
+++ .history/src/Pages/App_20220815164729.tsx
@@ -0,0 +1,18 @@
+import Header from '../Components/Header/Header';
+import logo from '../assets/images/';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Eu sou uma aplicação React</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
