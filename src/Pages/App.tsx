de5b69ce2a8d0557fdb5477633cc5f7fd5a2537e
Mariana Luiza Vitor Alves Silva <mariana.luiza@spcbrasil.org.br>
2022-08-16 14:55:46 -0300

    criação das rotas trazendo novo estilo

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from '../Components/Header/Header'
 import Contato from './Contato/Contato'
 import Home from './Home/Home'
-import NotFound from './NotFound/NotFouns'
 import Produtos from './Produtos/Produtos'
 import Sobre from './Sobre/Sobre'
 
@@ -20,7 +19,6 @@ function App() {
       <Route path="/sobre" element={<Sobre />} />
       <Route path="/produtos" element={<Produtos />} />
       <Route path="/contato" element={<Contato />} />
-      <Route path="*" element={<NotFound />} />
     </Routes>
     <Footer />
     </BrowserRouter>
